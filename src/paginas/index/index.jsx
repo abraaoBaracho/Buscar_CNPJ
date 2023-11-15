0a2c4ddbbba98ac38d5aacbc328db4238b5d03ce
abraaoBaracho <abraao-50@hotmail.com>
2023-11-15 20:22:34 -0300

    aplicando mudanças do tema do projeto

--- src/paginas/index/index.jsx
+++ src/paginas/index/index.jsx
@@ -1,36 +1,59 @@
 
 import '../../App.css'
-import google from '../../assets/google.png'
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { useState } from "react";
+
 
 function Index() {
+  const [cnpj, setCnpj] = useState("");
+  const [dados, setDados] = useState(null);
+  const [corTexto, setCorTexto] = useState("black");
+  const [tamanho, setTamanho] = useState(0);
+  const [mensagen, setMensagen] = useState("");
 
+  const inputCnpj = (e) => {
+    const inputValue = e.target.value;
+    setCnpj(inputValue);
+    setTamanho(inputValue.length)
+
+    if (inputValue.length < 14 || inputValue.length > 14) {
+      setMensagen("CNPJ invalido");
+      setCorTexto("red");
+    } else if(inputValue.length == 14) {
+      setMensagen("");
+      setCorTexto("green");
+    }else if(inputValue.length == 0){
+      setMensagen("");
+      setCorTexto("black");
+    }
+  };
+  
+  const buscar = () => {
+    
+     axios.get(`https://api-publica.speedio.com.br/buscarcnpj?cnpj=${cnpj}`)
+       .then(response => {
+         setDados(response.data)
+       })
+       .catch(error => {
+         console.error("Erro na busca do CNPJ", error);
+       });
+  };
 
   return (
     <>
       <div>
-        <h1>
-          Um aplicativo projetado especialmente para quem busca praticidade e economia ao realizar suas compras.
-        </h1>
-        <h3>
-          Em breve, o nosso aplicativo estará disponível.
-        </h3>
-        <a href="" >
-          <img id='google' src={google} alt="Erro" />
-        </a>
-        <h3>
-          Não fique só olhando venha fazer seu 
-          <Link to="/cadastro">
-           <span> Cadastro </span>
-          </Link>
-           ou 
-          <Link to="/login">
-            <span> Login</span>
-          </Link>
-        </h3>
-
+        <label htmlFor="cnpj">Digite o CNPJ que deseja buscar</label>
+        <br />
+        <input type="number" id='cnpj' onChange={inputCnpj} style={{ color: corTexto }} placeholder='digite apenas numeros'/>
+        <br />
+        <span style={{ color: corTexto }}>{mensagen}</span>
+        <p id='mostrar'style={{ color: corTexto }}>CNPJ: {cnpj} </p>
+        <button onClick={buscar}>Buscar</button>
       </div>
+    <div>
 
+    </div>
     </>
   )
 }
