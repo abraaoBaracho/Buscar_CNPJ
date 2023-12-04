abf0f59b5e7b3da922ee519efd6a131dd77745b7
Abraão H. Baracho <abraao-50@hotmail.com>
2023-12-04 18:04:59 -0300

    Update Img.jsx

--- src/componets/Img.jsx
+++ src/componets/Img.jsx
@@ -1,7 +1,7 @@
 import '../css/App.css'
 function Img() {
     return(
-        <img id="sigla" src="https://photos.google.com/search/_tra_/photo/AF1QipMpGUUolfVKGR5QRm9N1kW8jTyG5DUpUEUMPdFw" alt="erro" />
+        <img id="sigla" src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiIXWaZ0J8HSJkmlPhLdrEckegwcP7JTuPR3DbvXOB1AUgU5tVsDSK7XKDg31wG_aJ17d73LwNOEMfR810NnoLj9kYop98yPB5OqFjP3JfZm0DorXC5Nww04ZxN4h4NwgQTesuzPrIYeu91UZyHablxF5DeFc4_jk2eiNY2O0F1ivvHxumx854u-xtM/s320/logo.jpg alt="erro" />
     )
 }
 
