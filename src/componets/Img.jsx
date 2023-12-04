46ed44cf0a3ef051472340d13cc39a8f47389ae6
Abraão H. Baracho <abraao-50@hotmail.com>
2023-12-04 17:57:03 -0300

    Update Img.jsx

--- src/componets/Img.jsx
+++ src/componets/Img.jsx
@@ -1,7 +1,7 @@
 import '../css/App.css'
 function Img() {
     return(
-        <img id="sigla" src="src/img/siglaCNPJ.jpg" alt="erro" />
+        <img id="sigla" src="https://photos.google.com/search/_tra_/photo/AF1QipMpGUUolfVKGR5QRm9N1kW8jTyG5DUpUEUMPdFw" alt="erro" />
     )
 }
 
