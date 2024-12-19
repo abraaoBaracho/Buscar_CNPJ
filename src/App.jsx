5d1111b6c20aaa1c4f2aa7786412f60b4537a6ed
abraaoBaracho <abraao-50@hotmail.com>
2024-12-19 17:08:07 -0300

    melhoria no codigo

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import './css/App.css';
-import React, {useState } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import ButtonBusca from './componets/ButtonBusca';
-import { Table } from 'reactstrap';
-import { FormGroup, Label, FormFeedback, Input } from 'reactstrap';
+import { Table, FormGroup, Label, FormFeedback, Input } from 'reactstrap';
 import Img from './componets/Img';
 
 function App() {
-
   const [cnpj, setCnpj] = useState("");
   const [dados, setDados] = useState([]);
   const [tamanho, setTamanho] = useState(0);
@@ -15,70 +13,54 @@ function App() {
   const [botaoPadrao, setBotaoPadrao] = useState(true);
   const [inputErro, setInputErro] = useState(false);
   const [inputCorreto, setInputCorreto] = useState(false);
-  const [input, setInput] = useState(true);
 
   const inputCnpj = (e) => {
     const inputValue = e.target.value;
     setCnpj(inputValue);
-    setTamanho(inputValue.length)
+    setTamanho(inputValue.length);
 
     if (inputValue.length === 14) {
-      setInputErro(false)
-      setInput(false)
-      setInputCorreto(true)
-
-    } else if (inputValue.length === 0) {
-      setInputErro(false)
-      setInputCorreto(false)
-      setInput(true)
-
-    }  
+      setInputErro(false);
+      setInputCorreto(true);
+    } else {
+      setInputErro(inputValue.length > 0);
+      setInputCorreto(false);
+    }
   };
 
   const limpar = () => {
-    setCnpj("")
-    setDados([])
-    setInputErro(false)
-    setInput(false)
-    setInputCorreto(false)
-  }
+    setCnpj("");
+    setDados([]);
+    setInputErro(false);
+    setInputCorreto(false);
+  };
 
   const buscar = () => {
-
     if (tamanho === 14) {
       setBotaoAnimado(true);
       setBotaoPadrao(false);
 
-/* Basicamente, essa parte do código lida com a requisição para a API, 
-atualiza o estado da aplicação com os dados retornados e controla a exibição desses 
-dados na interface do usuário. */
-
-/* axios.get é o que faz a ligação do front ao back, quando apertarmos o botao pesquisar,
-ele vai buscar os dados associados e entregar ao usuario */
-
-      axios.get(`https://api-publica.speedio.com.br/buscarcnpj?cnpj=${cnpj}`)
-        .then(response => {
+      axios
+        .get(`https://api-publica.speedio.com.br/buscarcnpj?cnpj=${cnpj}`)
+        .then((response) => {
           setDados(response.data);
           setTimeout(() => {
             setBotaoAnimado(false);
             setBotaoPadrao(true);
-            
           }, 400);
         })
-        .catch(error => {
+        .catch((error) => {
           console.error("Erro na busca do CNPJ", error);
           setBotaoAnimado(false);
           setBotaoPadrao(true);
           setInputErro(true);
-          setInput(false);
           setInputCorreto(false);
         });
     } else {
-      alert("Erro, confira o CNPJ digitado")
+      alert("Erro, confira o CNPJ digitado.");
     }
-
-
   };
+
   const linhasTabela = Object.entries(dados).map(([chave, valor]) => (
     <tr key={chave}>
       <td><strong>{chave}</strong></td>
@@ -86,60 +68,51 @@ ele vai buscar os dados associados e entregar ao usuario */
     </tr>
   ));
 
-
   return (
     <>
-      <div id='img'>
-      <Img />
+      <div id="img">
+        <Img />
       </div>
-      
-      <div>
-        <form id='form'>
 
-          <Label for="cnpj" id='cnpjLabel'>
+      <div>
+        <form id="form">
+          <Label for="cnpjInput" id="cnpjLabel">
             Digite o CNPJ que deseja pesquisar
           </Label>
-          {input &&
-            <Input id='cnpj' onChange={inputCnpj} type='number' />
-          }
-          <br />
-
-          {inputCorreto &&
-            <FormGroup className="position-relative">
-              <Input valid id='cnpj' onChange={inputCnpj} type='number' value={cnpj} />
-            </FormGroup>
-          }
-
-          {inputErro &&
-            <FormGroup className="position-relative">
-              <Input invalid id='cnpj' onChange={inputCnpj} type='number' value={cnpj} />
-              <FormFeedback tooltip>
-                CNPJ invalido
-              </FormFeedback>
-            </FormGroup>
-          }
+          <FormGroup className="position-relative">
+            <Input
+              id="cnpjInput"
+              type="number"
+              value={cnpj}
+              onChange={inputCnpj}
+              valid={inputCorreto}
+              invalid={inputErro}
+            />
+            {inputErro && <FormFeedback tooltip>CNPJ inválido</FormFeedback>}
+          </FormGroup>
         </form>
-        <br />
         <div>
-          {botaoPadrao && <button id='buscar' onClick={buscar}><strong>Pesquisar</strong></button>}
+          {botaoPadrao && (
+            <button id="buscar" onClick={buscar}>
+              <strong>Pesquisar</strong>
+            </button>
+          )}
           {botaoAnimado && <ButtonBusca />}
-          <button id='limpar' onClick={limpar}>Limpar</button>
+          <button id="limpar" onClick={limpar}>
+            Limpar
+          </button>
         </div>
       </div>
 
-      <div id='tabela'>
-        <Table
-          hover
-        >
-          <tbody>
-            {linhasTabela}
-          </tbody>
+      <div id="tabela">
+        <Table hover>
+          <tbody>{linhasTabela}</tbody>
         </Table>
       </div>
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
 //39559195000140
